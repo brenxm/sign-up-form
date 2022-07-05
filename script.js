c13a56ed8fcb3b8d0da235b03530355c540e6270
brenxm <brymina18@yahoo.com>
2022-07-04 23:20:14 -0500

    all input stable and green checker initiate

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-import { inputValidator, applyInvalidMessage } from "./validation.mjs";
+import { inputValidator } from "./validation.mjs";
 
 const form = document.querySelector('.fieldset');
 const invalidTextElem = document.querySelectorAll('.invalid-alert');
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
-    const result = inputValidator(form);
-    if (result.proceedForm) return alert('Accepted Form, all valid inputs');
-
-    applyInvalidMessage(result, invalidTextElem);
+    const inputResult = inputValidator(form);
+    const validForm = Object.values(inputResult).every(obj => obj.valid == true);
+    console.log(inputResult);
+    if (validForm) sendForm();
+    else invalidForm(inputResult);
 });
+
+function sendForm(){
+    alert('Account succesfully created!, well not really but you made it');
+}
+
+function invalidForm(inputResult){
+    //get element of input and set red if invalid
+    //get text alert and set current text to current value
+    const arr = Object.entries(inputResult);
+
+    arr.map((arri)=>{
+        const input = document.querySelector(`#input-${arri[0]}`);
+        const textAlert = document.querySelector(`#invalid-${arri[0]}`);
+
+        if(arri[1].valid){
+            input.setAttribute('style','border: 2px solid var(--valid-highlight)');
+            textAlert.textContent = "";
+        }
+        else {
+            input.setAttribute('style', 'border: 2px solid var(--invalid-highlight)');
+            textAlert.textContent = arri[1].value;
+        }
+    });
+}
+
--- validation.mjs
+++ validation.mjs
@@ -1,55 +1,74 @@
 function inputValidator(formElement){
-    const validCollection = {};
-    const invalidCollection = {};
+    const collection = {
+        "first-name": {
+            value: "",
+            valid: false
+               },
+        "last-name": {
+            value: "",
+            valid: false
+              },
+        "phone-number": {
+            value: "",
+            valid: false
+            },
+        "email": {
+            value: "",
+            valid: false
+        },
+        "password": {
+            value: "",
+            valid: false
+        }
+    };
 
     //validates first name and last name •required, •max of 20 characters
     const nameInput = formElement['first-name'].value;
     const lastInput = formElement['last-name'].value;
     const nameRG = /^[a-z]{1,20}$/i;
-    if (nameInput.length != false && nameRG.test(nameInput)) acceptedInputs(nameInput, 'first-name', validCollection);
-    else invalidInputs(nameInput, 'first-name', invalidCollection);
+    if (nameInput.length != false && nameRG.test(nameInput)) acceptedInputs(nameInput, 'first-name', collection, true);
+    else acceptedInputs(invalidMessage(nameInput,"first-name"), 'first-name', collection, false);
 
-    if (lastInput != false && nameRG.test(lastInput)) acceptedInputs(lastInput, 'last-name', validCollection);
-    else invalidInputs(lastInput, 'last-name', invalidCollection);
+    if (lastInput != false && nameRG.test(lastInput)) acceptedInputs(lastInput, 'last-name', collection, true);
+    else acceptedInputs(invalidMessage(lastInput, "last-name"), 'last-name', collection, false);
 
     //validates phone number •required • accepted format (###) ### #### or ### ### ####, space or dash accepted
     const phoneFormat = /^[(]\d{3}[)][\s-]\d{3}[\s-]\d{4}$|^\d{3}[\s-]\d{3}[\s-]\d{4}$/;
     const phoneInput = formElement['phone-number'].value;
-    if (phoneInput != false && phoneFormat.test(phoneInput)) acceptedInputs(phoneInput, 'number', validCollection);
-    else invalidInputs(phoneInput, 'phone-number', invalidCollection);
+    if (phoneInput != false && phoneFormat.test(phoneInput)) acceptedInputs(phoneInput, 'phone-number', collection, true);
+    else acceptedInputs(invalidMessage(phoneInput, "phone-number"), 'phone-number', collection, false);
 
     //validates emails • required •proper email format
     const emailInput = formElement['email'].value;
     const emailFormat = /^\w+[@]\w+[\.]\w+/;
-    if (emailInput != false && emailFormat.test(emailInput)) acceptedInputs(emailInput, 'email', validCollection);
-    else invalidInputs(emailInput, 'email', invalidCollection);
+    if (emailInput != false && emailFormat.test(emailInput)) acceptedInputs(emailInput, 'email', collection, true);
+    else acceptedInputs(invalidMessage(emailInput, "email"), 'email', collection, false);
      
     //validate pw • required •must match •contain at least one uppercase, special char, and number
     const pwRegex = /[A-Z!@#$%^&*0-9]{3}/;
     const pwInput = formElement['password'].value;
     const secondPwInput = formElement['confirm-password'].value;
+    console.log(pwInput);
+    console.log(secondPwInput);
     if (pwInput.length > 7) 
         if (pwRegex.test(pwInput))        
             if (pwInput == secondPwInput)
-                acceptedInputs(pwInput,'password', validCollection);
-            else invalidInputs (3, 'password', invalidCollection);
-        else invalidInputs(2, 'password', invalidCollection);
-    else invalidInputs(1, 'password', invalidCollection);
+                acceptedInputs(pwInput,'password', collection, true);
+            else acceptedInputs(invalidMessage(3, "password"), 'password', collection, false);
+        else acceptedInputs(invalidMessage(2, "password"), 'password', collection, false);
+    else acceptedInputs(invalidMessage(1, "password"), 'password', collection, false);
 
 
-    return { 
-        valid: validCollection, 
-        invalid: invalidCollection,
-        proceedForm: Object.keys(invalidCollection).length == 0 ? true : false
-     };    
+    return collection;
 }
 
-function acceptedInputs(input, typeOfInput, accumulator){
-    accumulator[typeOfInput] = input;
+function acceptedInputs(input, typeOfInput, collection, valid){
+    collection[typeOfInput].value = input;
+    collection[typeOfInput].valid = valid;
 }
 
 //warning messages for invalid inputs
-function invalidInputs(input, typeOfInput, accumulator){
+function invalidMessage(input, typeOfInput){
     const errorMessages = [
         'First name is required', 
         'Invalid first name entered',
@@ -59,36 +78,38 @@ function invalidInputs(input, typeOfInput, accumulator){
         'Invalid phone number format entered',
         'Email is required',
         'Invalid email entered',
-        'Password is required',
+        'At least 8 character is required',
         'Password does not match',
         'Password must contain at least one\b• special character\b• uppercase letter\b•number'
     ]
+
+    let message;
+
     switch(typeOfInput){
         case 'first-name' :
-            input == false ? accumulator[typeOfInput] = errorMessages[0] : accumulator[typeOfInput] = errorMessages[1];
-            return;
+            input == false ? message = errorMessages[0] : message = errorMessages[1];
+            break;
         
         case 'last-name' :
-            input == false ? accumulator[typeOfInput] = errorMessages[2] : accumulator = errorMessages[3];
-            return;
+            input == false ? message = errorMessages[2] : message = errorMessages[3];
+            break;
         
         case 'phone-number' :
-            input == false ? accumulator[typeOfInput] = errorMessages[4] : accumulator = errorMessages[5];
-            return;
+            input == false ? message = errorMessages[4] : message = errorMessages[5];
+            break;
 
         case 'email' :
-            input == false ? accumulator[typeOfInput] = errorMessages[6] : accumulator = errorMessages[7];
-            return;
+            input == false ? message = errorMessages[6] : message = errorMessages[7];
+            break;
 
         case 'password' :
-            if (input == 1) return accumulator[typeOfInput] = errorMessages[8];
-            if (input == 2) return accumulator[typeOfInput] = errorMessages[9];
-            if (input == 3) return accumulator[typeOfInput] = errorMessages[10];
+            if (input == 1)  message = errorMessages[8];
+            if (input == 2)  message = errorMessages[10];
+            if (input == 3)  message = errorMessages[9];
     }
+
+    return message;
 }
 
-function applyInvalidMessage(result, elements){
-    console.log(elements);
-}
 
-export {inputValidator, applyInvalidMessage };
+export {inputValidator };
