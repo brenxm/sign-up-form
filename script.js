5eef7cc8110335832c28704ff5e6cab97adc4b1f
brenxm <brymina18@yahoo.com>
2022-07-05 11:35:13 -0500

    divert to iteration-B branch

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 import { inputValidator } from "./validation.mjs";
 
 const form = document.querySelector('.fieldset');
-const invalidTextElem = document.querySelectorAll('.invalid-alert');
+const inputName = document.querySelector('#input-first-name');
+
+
+inputName.addEventListener('input',()=>{console.log(`typing ${event.target.value}`)});
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
     const inputResult = inputValidator(form);
     const validForm = Object.values(inputResult).every(obj => obj.valid == true);
-    console.log(inputResult);
     if (validForm) sendForm();
     else invalidForm(inputResult);
 });
@@ -16,11 +18,9 @@ function sendForm(){
     alert('Account succesfully created!, well not really but you made it');
 }
 
-function invalidForm(inputResult){
-    //get element of input and set red if invalid
-    //get text alert and set current text to current value
-    const arr = Object.entries(inputResult);
 
+function invalidForm(inputResult){
+    const arr = Object.entries(inputResult);
     arr.map((arri)=>{
         const input = document.querySelector(`#input-${arri[0]}`);
         const textAlert = document.querySelector(`#invalid-${arri[0]}`);
@@ -36,3 +36,4 @@ function invalidForm(inputResult){
     });
 }
 
+//green arrow checker will check if it's 
